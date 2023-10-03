db7500209f1675ca3aa1a434f5251bb971146899
Amirametwally <amira.metwally56@gmail.com>
2023-10-03 22:14:35 +0300

    done

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 101):
     if i <= 9:
-        print("0{}".format(i), end=", ")
-    elif i > 9 and i <= 99:
-        print("{}".format(i), end=", ")
+        print("0{}".format(i), end=", "if i < 99 else "\n")
     else:
-        print("{}".format(i))
+        print("{}".format(i), end=", " if i < 99 else "\n")
