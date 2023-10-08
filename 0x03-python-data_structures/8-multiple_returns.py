6fb0e2c2f847f0969c18101e6c213911adf5104a
Amirametwally <amira.metwally56@gmail.com>
2023-10-08 23:11:34 +0300

    done

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,6 +4,7 @@ def multiple_returns(sentence):
     length = len(sentence)
     if length > 0:
         first_char = sentence[0]
+        return length, first_char
     else:
         return None
-    return length, first_char
+    
