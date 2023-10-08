295b4bdcef50ba32ac17a33675c6471c3cbf7872
Amirametwally <amira.metwally56@gmail.com>
2023-10-08 03:46:25 +0300

    done

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,9 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/python3
 def no_c(my_string):
-    return ''.join([char for char in my_string if char not in 'cC'])
+    string_list = list(my_string)
+    new_string = ''
+    for i in string_list:
+        if i == 'c' or i == 'C':
+            continue
+        new_string += i
+    return new_string
