6fce6ae5bbef8e164e1e2221d5ea078065d61daf
Amirametwally <amira.metwally56@gmail.com>
2023-10-10 03:35:42 +0300

    done

--- 0x03-python-data_structures/python.h
+++ 0x03-python-data_structures/python.h
@@ -1,7 +1,15 @@
 #ifndef PYTHON_H
 #define PYTHON_H
 
+#include <stdio.h>
 
+
+
+#include <assert.h>               // assert()
+#include <inttypes.h>             // uintptr_t
+#include <limits.h>               // INT_MAX
+#include <math.h>                 // HUGE_VAL
+#include <stdarg.h>
 typedef struct _object {
     _PyObject_HEAD_EXTRA
     Py_ssize_t ob_refcnt;
