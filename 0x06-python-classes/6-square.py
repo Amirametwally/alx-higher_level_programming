be99f68f2573d364de7836fb4a2f69b6b561531e
Amirametwally <amira.metwally56@gmail.com>
2023-10-25 02:35:21 +0300

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,15 +4,16 @@
 
 class Square:
     """square class"""
+
     def __init__(self, size=0, position=(0, 0)):
         """Initialize the Square instance with optional size."""
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
         """function that return size"""
-        return self.__size
+        return (self.__size)
 
     @size.setter
     def size(self, value):
@@ -27,7 +28,7 @@ class Square:
     @property
     def position(self):
         """get psition"""
-        return self.__position
+        return (self.__position)
 
     @position.setter
     def position(self, value):
