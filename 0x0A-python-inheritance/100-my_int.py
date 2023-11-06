ad4759c7f92b93f53bcd156d1bf54b6c0f415835
Amirametwally <amira.metwally56@gmail.com>
2023-11-06 23:31:45 +0200

    100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,10 +5,10 @@
 class MyInt(int):
     """MyInt is a rebel"""
 
-    def ___eq__(self, value):
+    def __eq__(self, value):
         """!= operator inverted"""
         return self.real != value
 
-    def ___ne__(self, value):
+    def __ne__(self, value):
         """== operator inverted"""
         return self.real == value
