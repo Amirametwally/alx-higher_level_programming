bc61496f4c9d56b712f58f88a4910720ed480008
Amirametwally <amira.metwally56@gmail.com>
2023-11-08 00:18:32 +0200

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""class student"""
+
+
+class Student:
+    """Public instance attributes"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Public method  that retrieves a dictionary
+        representation of a Student instance """
+
+        if attrs is None:
+            return self.__dict__
+        new_dictionary = {}
+        for key, value in self.__dict__.items():
+            for i in attrs:
+                if key == i:
+                    new_dictionary[key] = value
+        return new_dictionary
