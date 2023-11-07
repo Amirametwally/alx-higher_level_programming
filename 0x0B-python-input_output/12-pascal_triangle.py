359e4dd428634f64cfd344860fe3ef4ea8ed8545
Amirametwally <amira.metwally56@gmail.com>
2023-11-08 01:27:34 +0200

    search

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+    """function that returns a list of lists
+    of integers representing the Pascal's triangle of n"""
+
+    if n <= 0:
+        return []
+            
+    triangle = [[1]]
+    for i in range(1, n):
+        last_r = triangle[-1]
+        r = [1] + [last_r[j-1] + last_r[j] for j in range(1, i)] + [1]
+        triangle.append(r)
+    return triangle
