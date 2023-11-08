8fdde06cc19a5747d84852aa74bf99d4ee7c84bb
Amirametwally <amira.metwally56@gmail.com>
2023-11-08 03:41:28 +0200

    Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""script that reads stdin line by line and computes metrics"""
-
+"""Reads from standard input and computes metrics."""
 
 
 def print_stats(size, status_codes):
@@ -12,34 +11,33 @@ def print_stats(size, status_codes):
 if __name__ == "__main__":
     import sys
 
-
     size = 0
     status_codes = {}
     valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    counter = 0
-
+    count = 0
 
     try:
         for line in sys.stdin:
-            if counter == 10:
+            if count == 10:
                 print_stats(size, status_codes)
-                counter = 1
+                count = 1
             else:
-                counter += 1
+                count += 1
             line = line.split()
 
             try:
                 size += int(line[-1])
             except (IndexError, ValueError):
                 pass
-                try:
-                    if line[-2] in valid_codes:
-                        if status_codes.get(line[-2], -1) == -1:
-                            status_codes[line[-2]] = 1
-                        else:
-                            status_codes[line[-2]] += 1
-                except IndexError:
-                    pass
+
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
 
         print_stats(size, status_codes)
     except KeyboardInterrupt:
