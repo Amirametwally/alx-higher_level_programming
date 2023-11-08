36a4c76204aa0cd9d8dcb190dcaccdc7e61d598f
Amirametwally <amira.metwally56@gmail.com>
2023-11-08 03:43:41 +0200

    Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -8,21 +8,22 @@ def print_stats(size, status_codes):
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
 
+
 if __name__ == "__main__":
     import sys
 
     size = 0
     status_codes = {}
     valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    count = 0
+    counter = 0
 
     try:
         for line in sys.stdin:
-            if count == 10:
+            if counter == 10:
                 print_stats(size, status_codes)
-                count = 1
+                counter = 1
             else:
-                count += 1
+                counter += 1
             line = line.split()
 
             try:
@@ -42,4 +43,4 @@ if __name__ == "__main__":
         print_stats(size, status_codes)
     except KeyboardInterrupt:
         print_stats(size, status_codes)
-        raise  
+        raise
