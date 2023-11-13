e423138850b32c71b814267ca03e27db4a8612d1
Amirametwally <amira.metwally56@gmail.com>
2023-11-13 23:14:14 +0200

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,7 +91,7 @@ class Rectangle(Base):
 
     def update(self, *args, **kwargs):
         """updated"""
-        if args != None and len(args) != 0:
+        if args is not None and len(args) != 0:
             attributes = ["id", "width", "height", "x", "y"]
             for i in range(len(args)):
                 setattr(self, attributes[i], args[i])
