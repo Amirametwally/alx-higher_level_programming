6461faeaded4f4ccc9cd1fb1a51a0a9a73a8696e
Amirametwally <amira.metwally56@gmail.com>
2023-11-13 23:52:18 +0200

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,7 +9,7 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """initializes instances"""
-        super().__init__(self, size, x, y, id)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         str_one = f"[Rectangle] ({self.id}) {self.x}/{self.y}"
