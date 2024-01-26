18859b46013c707bbdc82bb1f6d7afb7d92a6fd6
Amirametwally <amira.metwally56@gmail.com>
2024-01-26 22:00:25 +0200

    Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-_suPython script that takes in a URL, sends a request
+Python script that takes in a URL, sends a request
 to the URL and displays the value of the X-Request-Id
-variable found in the header of the responsemmary_
+variable found in the header of the response
 """
 import urllib.request
 import sys
