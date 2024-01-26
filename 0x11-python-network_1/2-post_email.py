43ddd2f5f7cab36c91a5e971fc04146183fb19e3
Amirametwally <amira.metwally56@gmail.com>
2024-01-26 22:11:21 +0200

    POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,7 +2,7 @@
 """
 Python script that takes in a URL and an email,
 sends a POST request to the passed URL with the email
-as a parameter, and displays the body of the response 
+as a parameter, and displays the body of the response
 """
 from urllib import request, parse
 import sys
