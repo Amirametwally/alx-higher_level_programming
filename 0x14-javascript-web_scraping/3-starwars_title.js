046b303489a13550a8a73ff4b1ae7f5fd381f848
Amirametwally <amira.metwally56@gmail.com>
2024-02-20 23:38:50 +0200

    Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const r = require("request");
-const u = "http://swapi.co/api/films/" + process.argv[2];
+const r = require('request');
+const u = 'http://swapi.co/api/films/' + process.argv[2];
 r(u, (error, response, body) => {
   if (error) console.log(error);
   console.log(JSON.parse(body).title);
