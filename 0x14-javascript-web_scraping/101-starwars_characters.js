7560b7f1a5f0f280cce6e9697753fbc29755b5eb
Amirametwally <amira.metwally56@gmail.com>
2024-02-20 23:58:51 +0200

    Right order

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const r = require('request');
+const u = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+r(u, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
+});
+
+function printCharacters (characters, index) {
+  r(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
