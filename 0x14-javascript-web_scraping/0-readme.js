8f615b957d87b92ae2d1a320a62e131e21440889
Amirametwally <amira.metwally56@gmail.com>
2024-02-20 23:16:21 +0200

    Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-let fs = require("fs");
+const fs = require('fs');
 
-fs.readFile(process.argv[2], "utf-8", (err, buf) => {
+fs.readFile(process.argv[2], 'utf-8', (err, buf) => {
   if (err) console.log(err);
   else console.log(buf);
 });
