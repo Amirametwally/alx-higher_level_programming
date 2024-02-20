010622053178137dd2d95714e00451b42aa4b254
Amirametwally <amira.metwally56@gmail.com>
2024-02-20 23:12:32 +0200

    Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,10 +2,7 @@
 
 let fs = require("fs");
 
-fs.readFile(process.argv[2], 'utf-8', function (err, buf) {
-  if (!err) {
-    console.log(buf);
-  } else {
-    console.log(err);
-  }
+fs.readFile(process.argv[2], "utf-8", function (err, buf) {
+  if (err) console.log(err);
+  else console.log(buf);
 });
